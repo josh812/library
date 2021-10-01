7d9523ca23f7c1c5a22dfc19e447270664c2c9bd
Josh <joshua.mccrory@icloud.com>
2021-09-30 18:14:12 -0700

    Able to change the read status

--- script.js
+++ script.js
@@ -29,9 +29,25 @@ function displayLibrary(library) {
       let divPara = document.createElement("p");
       if(key === 'read') {
         if(value === true) {
+          let readCheckbox = document.createElement('input');
+          readCheckbox.setAttribute('id', 'read-checkbox');
+          readCheckbox.setAttribute('type', 'checkbox');
+          readCheckbox.setAttribute('data-title', library[i].title);
+          readCheckbox.checked = true;
           divPara.textContent = "Read";
+          divPara.setAttribute('id', 'read-para');
+          divPara.setAttribute('data-title', library[i].title);
+          div.appendChild(readCheckbox);
         } else {
+          let readCheckbox = document.createElement('input');
+          readCheckbox.setAttribute('id', 'read-checkbox');
+          readCheckbox.setAttribute('type', 'checkbox');
+          readCheckbox.setAttribute('data-title', library[i].title);
+          readCheckbox.checked = false;
           divPara.textContent = "Not Read";
+          divPara.setAttribute('id', 'read-para');
+          divPara.setAttribute('data-title', library[i].title);
+          div.appendChild(readCheckbox);
         }
       } else if(key === 'title') {
         divPara.textContent = `${value}`;
@@ -49,6 +65,7 @@ function displayLibrary(library) {
       div.appendChild(divPara);
       newBook.appendChild(div);
     }
+
     let buttonDiv = document.createElement('div');
     buttonDiv.classList.add('info');
 
@@ -64,6 +81,22 @@ function displayLibrary(library) {
 
     libraryDiv.appendChild(newBook);
   }
+  let readCheckboxes = document.querySelectorAll('#read-checkbox');
+  readCheckboxes.forEach(checkbox => {
+    checkbox.addEventListener('change', function() {
+      let readParas = document.querySelectorAll('#read-para');
+      readParas.forEach(readParagraph => {
+        if(readParagraph.dataset.title === checkbox.dataset.title) {
+          let readPara = readParagraph;
+          if(checkbox.checked) {
+            readPara.textContent = "Read";
+          } else if(!checkbox.checked) {
+            readPara.textContent = "Not Read";
+          }
+        }
+      });
+    });
+  });
 }
 function openForm() {
   document.getElementById("new-book-form").style.display = "block";
